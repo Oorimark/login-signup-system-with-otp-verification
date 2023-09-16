32a3130d3ab80bef050cbfe5e743daff02980d2f
Oori Mark <oorimark@gmail.com>
2023-09-16 01:23:11 +0100

    sorting received message collection

--- src/model/database/db.py
+++ src/model/database/db.py
@@ -1,3 +1,4 @@
+import datetime
 from werkzeug.security import generate_password_hash
 from config.config import clientMessagingCollection
 
@@ -6,6 +7,7 @@ class DatabaseModel:
     def __init__(self, collection):
         self.collection = collection
     def insert(self, data: dict):
+        # using middlewares with their associated collection
         match(self.collection.__name__):
             case 'user_collection':
                 data = UserCollectionMIddlewaresFactory.pre('insert', data)
@@ -25,10 +27,20 @@ class UserCollectionMIddlewaresFactory:
                 return data
 
 class ClientMessagingCollection:
+    def pre(self, action, data):
+        """ middlewares to be used with model actions """
+        match(action):
+            case 'insert':
+                data['time_stamp'] = datetime.datetime()
+                return data
+
+class ClientMessagingCollection:
     collection = clientMessagingCollection
     def __init__(self, get_request_params):
         self.sender_id, self.receiver_id, self.prev_client_messages = get_request_params.values()
     def search_messages(self):
         search_res_1 = self.collection.find({'sender_id': self.sender_id}, {'receiver_id': self.receiver_id})
         search_res_2 = self.collection.find({'sender_id': self.receiver_id}, {'receiver_id': self.sender_id})
-        return search_res_1 + search_res_2
+        self.searched_res = search_res_1 + search_res_2
+    def sort_searched_messages(self):
+        """ sorts searched messages by date """
