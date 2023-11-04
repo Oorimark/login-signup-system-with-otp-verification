b010c47d0171bfe47d9f4cfc4be0c18d92fa0fa7
Oori Mark <oorimark@gmail.com>
2023-11-04 14:08:02 +0100

    Changed mode condition checker from match to if else

--- index.py
+++ index.py
@@ -9,10 +9,9 @@ from waitress import serve
 mode = "prod"
 
 if __name__ == "__main__":
-    match mode:
-        case "dev":
-            app.run(debug=True, port=3112)
-            logging.info('Starting application in dev mode')
-        case "prod":
-            serve(app, host="0.0.0.0", port=4020, threads=10)
-            logging.info('Starting application in production mode')
+    if mode == "dev":
+        app.run(debug=True, port=3112)
+        logging.info('Starting application in dev mode')
+    else:
+        serve(app, host="0.0.0.0", port=4020, threads=10)
+        logging.info('Starting application in production mode')
