24526f3012e8c207e53d6e6c774daf268c4a043a
Oori Mark <oorimark@gmail.com>
2024-04-14 02:38:49 +0100

    Changed the default sender of the mail to mailXender@gmail.com

--- src/config/config.py
+++ src/config/config.py
@@ -9,13 +9,9 @@ load_dotenv()
 
 connection_string = f"mongodb+srv://{os.getenv('DB_CONNECTION_USERNAME')}:{os.getenv('DB_CONNECTION_PASSWORD')}@cluster0.xcz3g.mongodb.net/?retryWrites=true&w=majority"
 
-# connection_string = (
-#     "mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb"
-# )
-
 try:
     cluster = MongoClient(connection_string)
-except Exception as e:
+except Exception as __:
     print("MongoClient is not connected to mongo url")
 else:
     db = cluster[os.getenv("APP_NAME") + "_DATA_HOUSE"]
@@ -34,5 +30,8 @@ class MAIL_CONFIG:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER: (os.getenv("APP_NAME"), os.getenv("MAIL_USERNAME"))
+    MAIL_DEFAULT_SENDER = (
+        os.getenv("APP_NAME"),
+        "mailXender@gmail.com",
+    )  # os.getenv("MAIL_USERNAME"))
     MAIL_ASCII_ATTACHMENTS = False
